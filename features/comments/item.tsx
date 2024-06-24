add4b712a3b5f0853f6a79b21c41bd7d144455f5
alphazero-wd <alphazero25811@gmail.com>
2024-06-24 15:36:15 +0700

    add truncate long comments

--- features/comments/item.tsx
+++ features/comments/item.tsx
@@ -2,12 +2,51 @@ import { formatDistanceToNowStrict } from "date-fns/formatDistanceToNowStrict";
 import { ProfileAvatar } from "../users/profile/avatar";
 import { Comment } from "./types";
 import { Markdown } from "../common/markdown";
+import { useMemo, useState } from "react";
+import { Button } from "../ui/button";
 
 interface CommentItem {
   comment: Comment;
 }
 
+const MAX_CHARS_TO_SHOW = 100;
+const MAX_LINES_TO_SHOW = 3;
+
 export const CommentItem = ({ comment }: CommentItem) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const lines = useMemo(
+    () =>
+      [...comment.content.matchAll(/<p>(.*?)<\/p>/g)].map((match) => match[0]),
+    [comment.content]
+  );
+
+  const shouldTruncateByCharacters = useMemo(
+    () => comment.content.length > MAX_CHARS_TO_SHOW,
+    [comment.content.length]
+  );
+
+  const shouldTruncateByLines = useMemo(
+    () => lines.length > MAX_LINES_TO_SHOW,
+    [lines]
+  );
+
+  const truncatedContent = useMemo(() => {
+    if (showMore) return comment.content;
+    const truncatedLineIndex = shouldTruncateByLines
+      ? MAX_LINES_TO_SHOW + 1
+      : lines.length;
+
+    const truncatedCharIndex = shouldTruncateByCharacters
+      ? MAX_CHARS_TO_SHOW + 1
+      : comment.content.length;
+
+    return lines
+      .slice(0, truncatedLineIndex)
+      .join("")
+      .slice(0, truncatedCharIndex);
+  }, [shouldTruncateByCharacters, comment.content, showMore]);
+
   return (
     <div className="relative flex gap-x-4 w-full">
       <ProfileAvatar username={comment.profiles.username} />
@@ -38,7 +77,16 @@ export const CommentItem = ({ comment }: CommentItem) => {
           </div>
         </div>
         <div className="w-full">
-          <Markdown content={comment.content} />
+          <Markdown content={truncatedContent} />
+          {(shouldTruncateByLines || shouldTruncateByCharacters) && (
+            <Button
+              onClick={() => setShowMore(!showMore)}
+              variant="link"
+              className="p-0 text-primary underline underline-offset-4 w-fit h-fit"
+            >
+              Show {!showMore ? "more" : "less"}
+            </Button>
+          )}
         </div>
       </div>
     </div>
